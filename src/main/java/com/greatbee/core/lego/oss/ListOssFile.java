82810fc28ea4412beb36efcdd993d70574d99181
肖兵成 <bingcheng.xiao@chinaredstar.com>
2018-06-25 11:03:30 +0800

    lego 优化

--- src/main/java/com/greatbee/core/lego/oss/ListOssFile.java
+++ src/main/java/com/greatbee/core/lego/oss/ListOssFile.java
@@ -24,7 +24,7 @@ import java.util.List;
  * @author xiaobc
  * @date 18/6/21
  */
-@Component("createOssStorage")
+@Component("listOssFile")
 public class ListOssFile extends OssBase{
     private static final Logger logger = Logger.getLogger(ListOssFile.class);
 
