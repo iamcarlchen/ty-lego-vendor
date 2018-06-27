5c2f55b11ada5731c2c0b9ab9bb19110c877ee42
肖兵成 <bingcheng.xiao@chinaredstar.com>
2018-06-27 09:47:41 +0800

    jssdk lego

--- src/main/java/com/greatbee/core/lego/wx/WxAuth.java
+++ src/main/java/com/greatbee/core/lego/wx/WxAuth.java
@@ -15,9 +15,9 @@ import org.apache.log4j.Logger;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 
 /**
  * SelectCustomSQL
@@ -32,11 +32,18 @@ public abstract class WxAuth implements ExceptionCode, Lego{
 
     protected static final long ERROR_LEGO_WX_Params_Null = 300022L;
 
+    protected static final long ERROR_LEFO_WX_Sign_Error = 300031L;
+
     private static final String WX_Open_Host="https://open.weixin.qq.com";
     private static final String WX_Open_Api_Host = "https://api.weixin.qq.com";
 
     protected static final String Input_Key_Wx_Open_App_Id = "openAppId";//微信公众号appID（公众号的唯一标识）
     protected static final String Input_Key_WX_Open_Secret = "openSecret";//微信secret
+
+    private String access_token;
+    private long lastUpdateToken = 0;
+    //    private long updateInterval = 1000 * 7200;
+    private long updateInterval = 1000 * 1000;
 
     /**
      * 构建tpl模板需要的请求参数
@@ -171,6 +178,185 @@ public abstract class WxAuth implements ExceptionCode, Lego{
         return userinfo;
     }
 
+
+
+    /**
+     * ************************************ 下面是 jssdk 接口
+     */
+
+    /**
+     * controller层调用：@RequestMapping(value = "/getSignature")
+     * <p/>
+     * 获取signature
+     *
+     * @return pipelineContext
+     */
+    protected JSONObject getSignature(String signUrl,String appId,String appSecret) throws DBException {
+        //get ticket
+        String jsapi_ticket = this._getTicket(appId,appSecret);
+        logger.info("[getSignature]jsapi_ticket" + jsapi_ticket);
+        //get sign
+        String nonce_str = _createNonceStr();
+        String timestamp = _createTimestamp();
+        String string1;
+        String signature = "";
+        //注意这里参数名必须全部小写，且必须有序
+        string1 = "jsapi_ticket=" + jsapi_ticket +
+                "&noncestr=" + nonce_str +
+                "&timestamp=" + timestamp +
+                "&url=" + signUrl;
+        logger.info("[getSignature]signUrl=" + signUrl);
+
+        try {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(string1.getBytes("UTF-8"));
+            //hex
+            signature = byteToHex(crypt.digest());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            logger.error(e);
+            logger.error(e.getMessage());
+            logger.error(e.toString());
+            throw new DBException(e.getMessage(),ERROR_LEFO_WX_Sign_Error);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            logger.error(e);
+            logger.error(e.getMessage());
+            logger.error(e.toString());
+            throw new DBException(e.getMessage(),ERROR_LEFO_WX_Sign_Error);
+        }
+
+        JSONObject sign = new JSONObject();
+        sign.put("url", signUrl);
+        sign.put("jsapi_ticket", jsapi_ticket);
+        sign.put("nonceStr", nonce_str);
+        sign.put("timestamp", timestamp);
+        sign.put("signature", signature);
+        sign.put("appId", appId);
+        logger.info("[getSignature]signature=" + sign.toString());
+        return sign;
+
+    }
+
+    /**
+     * 获取ticket
+     *
+     * @return
+     * @throws Exception
+     */
+    private String _getTicket(String appId,String appSecret) throws DBException {
+        String url = WX_Open_Api_Host + "/cgi-bin/ticket/getticket?access_token=" + this.getAccessTokenString(appId,appSecret)+"&type=jsapi";
+        //get ticket
+        String httpResponse = HttpClientUtil.get(url,null).getResponseBody();
+        logger.info(httpResponse);
+        if (StringUtil.isInvalid(httpResponse)) {
+            logger.error("输出jsapi：" +"[SimpleWxEnterpriseManager][_getTicket][error] httpResponse is invalid!");
+        }
+        JSONObject jsonObject = JSONObject.parseObject(httpResponse);
+        if (jsonObject.containsKey("ticket")) {
+            return jsonObject.getString("ticket");
+        } else {
+            logger.error("[SimpleWxEnterpriseManager][_getTicket][error] httpResponse is invalid!");
+            return null;
+        }
+    }
+
+    /**
+     * 获取accessToken的url
+     *
+     * @return
+     */
+    protected String getAccessTokenUrl(String appId,String appSecret) {
+        StringBuilder urlBuilder = new StringBuilder(WX_Open_Api_Host);
+        urlBuilder.append("/cgi-bin/token?");
+        urlBuilder.append("grant_type=client_credential");
+        urlBuilder.append("&appid=").append(appId);
+        urlBuilder.append("&secret=").append(appSecret);
+        return urlBuilder.toString();
+    }
+
+    /**
+     * 只需要access_token有值就可以了，不需要返回，获取access_token值
+     *
+     * @throws LegoException
+     */
+    protected void initAccessToken(String appId,String appSecret) throws DBException {
+        String url = this.getAccessTokenUrl(appId,appSecret);
+        String httpResponse = HttpClientUtil.get(url,null).getResponseBody();
+        if (StringUtil.isInvalid(httpResponse)) {
+            logger.error("[SimpleWxEnterpriseManager][initAccessToken][error] httpResponse is invalid!");
+        }
+        JSONObject jsonObject = JSONObject.parseObject(httpResponse);
+        if (jsonObject.containsKey("access_token")) {
+            access_token = jsonObject.getString("access_token");
+            //更新lastupdate时间戳,以免暴力请求导致sdk不能使用
+            lastUpdateToken = System.currentTimeMillis();
+            System.out.println(access_token);
+        }
+    }
+
+
+    /**
+     * 获取accesstoken的值，添加点击频率校验，以免暴力请求导致sdk不能使用
+     *
+     * @return
+     * @throws Exception
+     */
+    protected String getAccessTokenString(String appId,String appSecret) throws DBException {
+        if (accessTokenInvalid()) {
+            initAccessToken(appId,appSecret);
+        }
+        return access_token;
+    }
+    /**
+     * 校验，以免暴力请求导致sdk不能使用
+     *
+     * @return
+     */
+    private boolean accessTokenInvalid() {
+        if (StringUtil.isInvalid(access_token)) {
+            return true;
+        } else if (lastUpdateToken + updateInterval < System.currentTimeMillis()) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 创建随机数
+     *
+     * @return
+     */
+    private String _createNonceStr() {
+        return UUID.randomUUID().toString();
+    }
+
+    /**
+     * 创建当前时间戳
+     *
+     * @return
+     */
+    private String _createTimestamp() {
+        return Long.toString(System.currentTimeMillis() / 1000);
+    }
+
+    /**
+     * 微信消息需要转码
+     *
+     * @param hash
+     * @return
+     */
+    private String byteToHex(final byte[] hash) {
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
 
 
 
