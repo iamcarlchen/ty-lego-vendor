01aa88bcc886783bdb5d1e6d87f6bee2944aafbc
xiaobc <527710279@qq.com>
2018-08-02 13:01:31 +0800

    新增  级联新增lego

---
+++ src/main/java/com/greatbee/core/utils/VendorExceptionCode.java
@@ -0,0 +1,16 @@
+package com.greatbee.core.utils;
+
+/**
+ * VendorExceptionCode vendor的所有异常code  后面vendr的异常code 都在这里维护
+ *
+ * @author xiaobc
+ * @date 18/7/31
+ */
+public class VendorExceptionCode {
+
+
+    public static final long Lego_Error_Index_data_Null = 300070L;
+    public static final long Lego_Error_CascadeAdd_Create_Field_Null = 300071L;
+
+
+}
