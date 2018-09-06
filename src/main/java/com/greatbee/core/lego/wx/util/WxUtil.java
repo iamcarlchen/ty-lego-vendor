13e1913fc77e007d8ed0f9ddd27554a29845e552
xiaobc <527710279@qq.com>
2018-09-06 20:55:30 +0800

    小蚁签名组件 完成 微信支付组件 初始化 因为小蚁授权ip在服务器上，测试必须在服务器上，版本升级有点快

--- src/main/java/com/greatbee/core/lego/wx/util/WxUtil.java
+++ src/main/java/com/greatbee/core/lego/wx/util/WxUtil.java
@@ -1,17 +1,24 @@
 package com.greatbee.core.lego.wx.util;
 
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.greatbee.base.bean.DBException;
+import com.greatbee.base.util.DataUtil;
+import com.greatbee.base.util.MD5Util;
 import com.greatbee.base.util.StringUtil;
 import com.greatbee.core.lego.LegoException;
 import com.greatbee.core.util.HttpClientUtil;
 import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Formatter;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * WxUtil jssdk
@@ -26,6 +33,12 @@ public class WxUtil {
     protected static final long ERROR_LEFO_WX_Sign_Error = 300031L;
 
     private static final String WX_Open_Api_Host = "https://api.weixin.qq.com";
+
+    private static final String WX_Merchant_Host = "https://api.mch.weixin.qq.com";
+
+    private static final String Device_Info = "WEB";//PC网页或者公众号支付用WEB
+    public final static String WEIXIN_ORDER_STATUS_SUCCESS = "SUCCESS";
+    public final static String WEIXIN_API_SIGN_TYPE = "MD5";
 
     private static String access_token;
     private static long lastUpdateToken = 0;
@@ -157,7 +170,7 @@ public class WxUtil {
      */
     public static String getAccessTokenString(String appId,String appSecret) throws DBException {
         if (accessTokenInvalid()) {
-            initAccessToken(appId,appSecret);
+            initAccessToken(appId, appSecret);
         }
         return access_token;
     }
@@ -208,6 +221,264 @@ public class WxUtil {
         String result = formatter.toString();
         formatter.close();
         return result;
+    }
+
+
+    /**
+     * 统一下单接口  之前授权过，直接传openId
+     * * 统一下单接口参数：
+     * {
+     *     appid:公众账号ID,
+     *     mch_id:商户号,
+     *     device_info:'WEB',
+     *     nonce_str:随机字符串,
+     *     sign:签名,
+     *     sign_type:'MD5',
+     *     body:商品描述,
+     *     detail:商品详情,
+     *     attach:附加数据,
+     *     out_trade_no:商户订单号,
+     *     fee_type:"CNY",//标价币种
+     *     total_fee:标价金额,单位为分,
+     *     spbill_create_ip:终端IP,
+     *     time_start:交易起始时间,
+     *     time_expire:交易结束时间	,
+     *     goods_tag:订单优惠标记,
+     *     notify_url:通知地址,
+     *     trade_type:"JSAPI",//交易类型
+     *     product_id:商品ID	,NATIVE时必传
+     *     openid:用户标识
+     * }
+     *
+     * @param params
+     * params:{
+     *     appid:'',
+     *     serialNumber:'订单编号',
+     *     mch_id:'商户id',
+     *     notify_url:'通知地址',
+     *     openid:'用户openid',
+     *     total_fee:'总金额，单位元',
+     *     trade_type:'交易类型，默认JSAPI',
+     *     wx_api_secret:'微信api密钥',
+     *     body:'商品描述'//没有就显示订单号
+     * }
+     * @return
+     * @throws Exception
+     */
+    public static JSONObject weixinOrder(Map<String,Object> params) throws Exception{
+        String url = WX_Merchant_Host + "/pay/unifiedorder";
+        Map<String,String> map = _getWxOrderParam(params);
+        JSONObject httpResponse = wxPost(url, map);
+        //接口请求失败
+        if(httpResponse.containsKey("return_code") && StringUtil.equals(httpResponse.get("return_code").toString(),"FAIL")){
+            throw new LegoException(httpResponse.get("return_msg").toString(),300112L);
+        }
+
+        Map<String,String> payMap = new TreeMap<String,String>();
+
+        //如果return_code 和result_code 都为SUCCESS时  说明请求成功
+        if(httpResponse.containsKey("return_code")
+                &&httpResponse.containsKey("result_code")
+                &&StringUtil.equals(httpResponse.get("return_code").toString(),WEIXIN_ORDER_STATUS_SUCCESS)
+                &&StringUtil.equals(httpResponse.get("result_code").toString(),WEIXIN_ORDER_STATUS_SUCCESS)){
+            if(httpResponse.containsKey("prepay_id")){
+                String prepayId = httpResponse.get("prepay_id").toString();
+                payMap.put("package","prepay_id="+prepayId.trim());
+            }
+        }else{
+            throw new LegoException("请求失败，"+httpResponse.get("return_msg"),300113L);
+        }
+        payMap.put("appId",DataUtil.getString(params.get("appid"),"").trim());
+        String timeStamp = _createTimestamp();
+        payMap.put("timeStamp",timeStamp.trim());//时间戳
+        String nonce_str_tmp = _createNonceStr();
+        String nonce_str = nonce_str_tmp.replaceAll("-","").substring(0, 32);//随机数
+        payMap.put("nonceStr", nonce_str.trim());
+        payMap.put("signType",WEIXIN_API_SIGN_TYPE);
+        String paySign = _wxPaySign(payMap,DataUtil.getString(params.get("wx_api_secret"),"").trim());
+        payMap.put("paySign", paySign.trim());
+
+        JSONObject resultJson = JSONObject.parseObject(JSONObject.toJSONString(payMap));
+
+        return resultJson;
+    }
+
+    /**
+     * 微信支付接口  签名
+     * @param map
+     * @return
+     */
+    private static String _wxPaySign(Map<String,String> map,String wxApiSecret) throws Exception {
+        String stringTmp;
+        String signature = "";
+        //注意这里参数名必须全部小写，且必须有序
+        stringTmp = "appId=" + map.get("appId") +
+                "&nonceStr=" + map.get("nonceStr") +
+                "&package=" + map.get("package") +
+                "&signType=" + map.get("signType") +
+                "&timeStamp=" + map.get("timeStamp") +
+                "&key=" + wxApiSecret.trim();
+        logger.info("[getSignature]stringTmp=" + stringTmp);
+        signature = MD5Util.getMD5(stringTmp.trim());
+        return signature.toUpperCase();
+    }
+
+    /**
+     * 获取统一下单接口 参数 包括签名   已经授权过一次，直接传给openid
+     * @param params
+     * params:{
+     *     appid:'',
+     *     serialNumber:'订单编号',
+     *     mch_id:'商户id',
+     *     notify_url:'通知地址',
+     *     openid:'用户openid',
+     *     total_fee:'总金额，单位元',
+     *     trade_type:'交易类型，默认JSAPI',
+     *     wx_api_secret:'微信api密钥',
+     *     body:'商品描述'//没有就显示订单号
+     * }
+     * @return
+     * @throws Exception
+     */
+    private static Map<String,String> _getWxOrderParam(Map<String,Object> params) throws Exception {
+        //get sign
+        String nonce_str_tmp = _createNonceStr();
+        String nonce_str = nonce_str_tmp.replaceAll("-","").substring(0,32);
+
+        Map<String,String> map = new TreeMap<String,String>();
+        map.put("appid", DataUtil.getString(params.get("appid"),"").trim());
+        map.put("body", DataUtil.getString(params.get("body"),DataUtil.getString(params.get("serialNumber"), "")).trim());
+        map.put("device_info",Device_Info);
+        map.put("mch_id", DataUtil.getString(params.get("mch_id"),"").trim());
+        map.put("nonce_str",nonce_str);
+        map.put("notify_url",DataUtil.getString(params.get("notify_url"),"").trim());
+        map.put("openid", DataUtil.getString(params.get("openid"),"").trim());
+        map.put("out_trade_no",DataUtil.getString(params.get("serialNumber"), "").trim());
+//        map.put("spbill_create_ip",context.getRequest().getHttpServletRequest().getRemoteAddr().trim());
+        map.put("spbill_create_ip","192.168.1.100");
+
+        double moneyTmp = mul(DataUtil.getDouble(params.get("total_fee"), 0), 100);
+        int money = DataUtil.getInt(moneyTmp,0);
+        map.put("total_fee", String.valueOf(money));
+
+        //    map.put("total_fee","1");
+        map.put("trade_type",DataUtil.getString(params.get("trade_type"),"JSAPI"));
+
+        String stringTmp;
+        String signature = "";
+        //注意这里参数名必须全部小写，且必须有序
+        stringTmp = "appid=" + map.get("appid") +
+                "&body=" + map.get("body") +
+                "&device_info=" + Device_Info +
+                "&mch_id=" + map.get("mch_id") +
+                "&nonce_str=" + nonce_str +
+                "&notify_url=" + map.get("notify_url") +
+                "&openid=" + map.get("openid") +
+                "&out_trade_no=" + map.get("out_trade_no") +
+                "&spbill_create_ip=" + map.get("spbill_create_ip") +
+                "&total_fee=" + map.get("total_fee") +
+                "&trade_type=" + map.get("trade_type") +
+                "&key=" + DataUtil.getString(params.get("wx_api_secret"),"").trim();
+        logger.info("[getSignature]stringTmp=" + stringTmp);
+        signature = MD5Util.getMD5(stringTmp);
+        map.put("sign",signature.toUpperCase());
+        return map;
+    }
+
+
+    /**
+     * 提供精确的乘法运算。
+     *
+     * @param v1 被乘数
+     * @param v2 乘数
+     * @return 两个参数的积
+     */
+    public static double mul(double v1, double v2) {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.multiply(b2).doubleValue();
+    }
+
+    /**
+     * WXPOST
+     *
+     * @param url
+     * @param params
+     * @return
+     */
+    public static JSONObject wxPost(String url, Map<String, String> params) throws DBException {
+        logger.info("[http][request]url=" + url + ",params=" + JSONObject.toJSONString(params).toString());
+        String xmlStr = _mapToXMLStr(params);
+        String response = HttpClientUtil.sendPostBody(url, xmlStr);
+        logger.info("[http][response]" + response);
+        try {
+            // String resStr = new String(response.getBytes("ISO-8859-1"),"UTF-8");
+            return _xmlToJSON(response);
+        } catch (JSONException e) {
+            System.out.println(e.getMessage());
+            System.out.println("[response][" + response.getBytes().length / 1000 + "KB]" + response);
+        }
+        return null;
+    }
+
+    /**
+     * map对象转换成xml字符串
+     *
+     * @param map
+     * @return
+     */
+    private static String _mapToXMLStr(Map<String, String> map) {
+        Document document = DocumentHelper.createDocument();
+        Element root = document.addElement("xml");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (!StringUtil.equals(entry.getKey(), "sign")) {
+                Element elm = root.addElement(entry.getKey());
+                elm.setText(entry.getValue());
+            }
+        }
+        Element elms = root.addElement("sign");
+        elms.setText(map.get("sign"));
+        return document.asXML();
+    }
+
+    /**
+     * map对象转换成xml字符串   不要特殊处理
+     *
+     * @param map
+     * @return
+     */
+    private static String _mapToXMLStrNew(Map<String, String> map) {
+        Document document = DocumentHelper.createDocument();
+        Element root = document.addElement("xml");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            Element elm = root.addElement(entry.getKey());
+            elm.setText(entry.getValue());
+        }
+        return document.asXML();
+    }
+
+    /**
+     * 解析微信传过来的 xml数据
+     *
+     * @param xmlStr
+     * @return
+     */
+    private static JSONObject _xmlToJSON(String xmlStr) {
+        try {
+            Document document = DocumentHelper.parseText(xmlStr);
+            //获取文档的根节点
+            Element root = document.getRootElement();
+            Map<String, String> map = new HashMap<String, String>();
+            //对某节点下的所有子节点进行遍历.
+            for (Iterator it = root.elementIterator(); it.hasNext(); ) {
+                Element element = (Element) it.next();
+                map.put(element.getName(), element.getText());
+            }
+            return JSONObject.parseObject(JSONObject.toJSONString(map));
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
