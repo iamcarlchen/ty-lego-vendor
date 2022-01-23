5a00555b9f156cc6791ba851f91ebf9418b918e6
xiaobc <527710279@qq.com>
2022-01-23 18:59:36 +0800

    启动报错的问题

--- src/main/java/com/greatbee/core/utils/rsaJavaTransferNet/Base64Helper.java
+++ src/main/java/com/greatbee/core/utils/rsaJavaTransferNet/Base64Helper.java
@@ -1,6 +1,6 @@
 package com.greatbee.core.utils.rsaJavaTransferNet;
 
-
+import org.apache.commons.codec.binary.Base64;
 import java.io.IOException;
 
 /**
@@ -12,16 +12,10 @@ import java.io.IOException;
 public class Base64Helper {
 
     public static String encode(byte[] byteArray) {
-        sun.misc.BASE64Encoder base64Encoder = new sun.misc.BASE64Encoder();
-        return base64Encoder.encode(byteArray);
+        return Base64.encodeBase64String(byteArray);
     }
 
     public static byte[] decode(String base64EncodedString) {
-        sun.misc.BASE64Decoder base64Decoder = new sun.misc.BASE64Decoder();
-        try {
-            return base64Decoder.decodeBuffer(base64EncodedString);
-        } catch (IOException e) {
-            return null;
-        }
+        return Base64.decodeBase64(base64EncodedString);
     }
 }
